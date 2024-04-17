ac9451ddbf24ebe2f8d7d39b72467339c75c408c
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-04-17 18:21:29 +0200

    updated db and postman

--- docs/zenit_back.sql
+++ docs/zenit_back.sql
@@ -15,6 +15,8 @@ CREATE TABLE IF NOT EXISTS models(
       title VARCHAR(50) NOT NULL,
       description VARCHAR(255) NOT NULL,
       technologies VARCHAR(255) NOT NULL,
+      category1 ENUM('Personal', 'Professional') NOT NULL,
+      category2 ENUM('Cartoon', 'Realistic', 'Stylized') NOT NULL,
       createdAt DATETIME NOT NULL DEFAULT NOW()
     );
 
