d62931335ca42f130272704d52e1d66cade1e514
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-04-17 16:51:17 +0200

    fixed helpers and created verification by email

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,11 @@
 const generateError = require('./generateError');
 const sendMail = require('./sendMail');
 const createPathIfNotExists = require('./createPathIfNotExists');
+const verifyEmail = require('./verifyEmail');
 
-module.exports = { generateError, sendMail, createPathIfNotExists };
+module.exports = {
+  generateError,
+  sendMail,
+  createPathIfNotExists,
+  verifyEmail,
+};
