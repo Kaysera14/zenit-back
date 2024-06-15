aacdae74e2e3da1b428102cbaa8829e97dd667eb
Kaysera0 <kaysera0@icloud.com>
2024-06-15 22:48:59 +0200

    uploading images to railway

--- controllers/models/newModel.js
+++ controllers/models/newModel.js
@@ -51,16 +51,16 @@ const newModel = async (req, res, next) => {
       let cover;
       const uuid = randomUUID();
       const newName = `${url}__${uuid}.webp`;
-      const directory = process.env.RAILWAY_VOLUME_MOUNT_PATH;
-      await createPathIfNotExists(directory);
+      const directory = process.env.RAILWAY_VOLUME_MOUNT_PATH || '/app/models';
+      /* await createPathIfNotExists(directory);
       const models = path.join(directory, 'models');
-      await createPathIfNotExists(models);
-      const imgUrl = `${process.env.RAILWAY_VOLUME_MOUNT_PATH}/models/${newName}`;
+      await createPathIfNotExists(models); */
+      const imgUrl = `${directory}/${newName}`;
 
       await sharp(images[index].data)
         .toFormat('webp')
         .webp({ effort: 6, quality: 80 })
-        .toFile(path.join(models, newName), (err) => {
+        .toFile(path.join(directory, newName), (err) => {
           if (err) {
             console.error(err);
           }
