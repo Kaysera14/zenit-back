484608497e20b284e63bd86f758c4dca77488302
Kaysera0 <kaysera0@icloud.com>
2024-06-27 00:03:07 +0200

    changed the server.js file

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ app.use(express.json());
 app.use(fileUpload());
 
 // Middleware para cargar imagenes railway
-/* let staticPath = process.env.RAILWAY_VOLUME_MOUNT_PATH;
-app.use(process.env.RAILWAY_VOLUME_MOUNT_PATH, express.static(staticPath)); */
+let staticPath = process.env.RAILWAY_VOLUME_MOUNT_PATH;
+app.use(process.env.RAILWAY_VOLUME_MOUNT_PATH, express.static(staticPath));
 
 // Middleware para cargar imagenes local
-app.use('/uploads/models', express.static('./uploads/models'));
+/* app.use('/uploads/models', express.static('./uploads/models')); */
 
 // Controllers usuarios
 const { register, validate, login } = require('./controllers/users/index.js');
